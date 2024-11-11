86157469806d659f77b62bb27b4670c2908597af
Ceprnic <ceprnic.josip@gmail.com>
2024-11-11 20:02:54 +0100

    apply dfs algorithm

--- task04/exercise03.py
+++ task04/exercise03.py
@@ -1,9 +1,25 @@
 def isEuler(graph):
     for vertex in graph:
-        # print(graph[vertex])
         if len(graph[vertex]) % 2 != 0:
             return False
-            '''ostatak'''
+    visited = set()
+
+    def dfs(v):
+        visited.add(v)
+        for neighbour in graph[v]:
+            print("v", v)
+            print(neighbour)
+            print(graph[v])
+            if neighbour not in visited:
+                print("neighbour not visited", neighbour)
+                dfs(neighbour)
+    # pocetni vrh
+    startVertex = list(graph.keys())[0]
+    print("start", startVertex)
+    dfs(startVertex)
+
+    if len(visited) != len(graph):
+        return False
     return True
 
 
@@ -17,14 +33,23 @@ def main():
     # }
 
     graph = {
-        1: [2, 5],
-        2: [1, 5],
-        3: [4, 5],
-        4: [3, 5],
+        1: [2, 5], 
+        2: [1, 5], 
+        3: [4, 5], 
+        4: [3, 5], 
         5: [1, 2, 4, 3]
     }
-    
+
+    # graph = {
+    #     1: [2, 5],
+    #     2: [1, 5],
+    #     3: [4, 5],
+    #     4: [3, 5],
+    #     5: [1, 2, 4, 3]
+    # }
+
     print(isEuler(graph))
+
 
 if __name__ == "__main__":
     main()
